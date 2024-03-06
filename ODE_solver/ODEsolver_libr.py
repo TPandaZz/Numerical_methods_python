86a88b02d39480f9109441f97271b057c4e93b34
Michal Sleszynski <michal.sleszynski2@gmail.com>
2024-03-06 15:44:33 +0000

    ode solver update

--- ODE_solver/ODEsolver_libr.py
+++ ODE_solver/ODEsolver_libr.py
@@ -3,6 +3,11 @@
 Created on Feb 25
 
 @author: Michal Sleszynski
+
+We are approximating the solution to an
+initial value problem between 0 and 4 
+in increments of 0.1 using the 
+Explicity Euler Formula (Method to Solve ODE's)
 """
 
 import numpy as np
@@ -10,9 +15,9 @@ import matplotlib.pyplot as plt
 
 # Define parameters
 f = lambda t, s: np.exp(t) # ODE
-h = 0.1 # Step size
+h = 0.1 # Step size (increase for better result)
 t = np.arange(0, 4 + h, h) # Numerical grid
-s0 = -1 # Initial Condition
+s0 = 1 # Initial Condition
 
 # Explicit Euler Method
 s = np.zeros(len(t))
@@ -21,9 +26,11 @@ s[0] = s0
 for i in range(0, len(t) - 1):
     s[i + 1] = s[i] + h*f(t[i], s[i])
 
-# Plot 
+
+# Ploting the difference between the approximate
+# solution and the exact solution
 plt.figure(figsize = (12, 7))
-plt.plot(t, s, 'ro--', label='Approximate')
+plt.plot(t, s, 'r--', label='Approximate')
 plt.plot(t, np.exp(t), 'b', label='Exact')
 plt.title('Approximate and Exact Solution for Simple ODE')
 plt.xlabel('t')
